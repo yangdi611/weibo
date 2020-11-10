7891718b5beaf7fd2ca3a70e23cc514a53c13bdd
vagrant <vagrant@homestead>
2020-11-10 05:49:46 +0000

    user

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -3,7 +3,7 @@
     <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
       <li><a href="{{ route('help') }}">帮助</a></li>
-      <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+      <li><a href="#">登录</a></li>
     </ul>
   </div>
 </nav>
